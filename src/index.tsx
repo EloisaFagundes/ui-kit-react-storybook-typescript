35b6fb5731f7a0b5fdf0ed25bd7dc35d4f09e35e
EloisaFagundes <lola.fernanda@hotmail.com>
2020-09-20 20:21:59 -0300

    "add exportação dos componentes para as stories"

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
 // Eu não uso esse arquivo como ponto de partida da aplicação, uso ele como ponto de partida 
 // para o dev importar os dados. (para importar no npm)
 export { Button } from "./components/Button"
+export { Separator } from "./components/Separator"
+export { Input } from "./components/Input "
 
