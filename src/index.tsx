1aaf3a9669dd6a90cf499e0cd58f964d03c8dd1a
EloisaFagundes <lola.fernanda@hotmail.com>
2020-09-20 12:16:57 -0300

    criando o componente de botão

---
+++ src/index.tsx
@@ -0,0 +1,4 @@
+// Eu não uso esse arquivo como ponto de partida da aplicação, uso ele como ponto de partida 
+// para o dev importar os dados. (para importar no npm)
+export { Button } from "./components/Button"
+
