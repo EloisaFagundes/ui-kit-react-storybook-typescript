21f9c9ec5b5afd8555e433b27a70083fb91404a2
EloisaFagundes <lola.fernanda@hotmail.com>
2020-09-20 20:19:36 -0300

    "adiciona componente Button"

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,16 +1,31 @@
 import React from "react";
+import { Container } from "./styles"
 
-const style: React.CSSProperties = {
-    backgroundColor: '#7159c1',
-    color: '#fff'
-}
-
-export const Button: React.FC = ({children}) => {
+export interface Props {
+    backgroundColor?: string;
+    color?: string;
+    outlined?: boolean;
+    onClick?: () => void;
+    disabled?: boolean;
+  }
+  
+ const ButtonWrapper: React.FC<Props> = ({
+    children,
+    backgroundColor = '#7159c1',
+    color = '#fff',
+    outlined = false,
+    ...props
+  }) => {
     return (
-        <>
-        <button style={style}>
-            {children}
-        </button>
-        </>
-    )
-}
+      <Container
+        backgroundColor={backgroundColor}
+        color={color}
+        outlined={outlined}
+        {...props}
+      >
+        {children}
+      </Container>
+    );
+  };
+
+  export { ButtonWrapper as Button };
