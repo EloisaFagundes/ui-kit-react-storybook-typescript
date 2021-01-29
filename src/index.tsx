5ccb9c4cc4827973ef4dddba0a8ed41783b71d0a
EloisaFagundes <lola.fernanda@hotmail.com>
2021-01-29 20:42:37 -0300

    fix: change Separator to Divider

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 // Eu não uso esse arquivo como ponto de partida da aplicação, uso ele como ponto de partida 
 // para o dev importar os dados. (para importar no npm)
-export { Button } from "./components/Button"
-export { Separator } from "./components/Separator"
-export { Input } from "./components/Input "
+export { Button } from "./components/Button";
+export { Divider } from "./components/Divider";
+export { Input } from "./components/Input ";
+export { Card } from "./components/Card";
+export { RiIcon } from "./components/RiIcon"
 
